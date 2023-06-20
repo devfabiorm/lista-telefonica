80c282f42d86ea6d285ecba45657d972b0c5ffe1
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2023-06-20 14:13:18 -0300

    scaffolding to have a better order

---
+++ js/controllers/listaTelefonicaController.js
@@ -0,0 +1,55 @@
+angular
+  .module("listaTelefonica")
+  .controller("listaTelefonicaCtrl", function ($scope, $http, uppercaseFilter) {
+    $scope.app = "Lista Telefônica";
+    $scope.contatos = [];
+    $scope.operadoras = [];
+
+    var carregarContatos = function () {
+      $http.get("http://localhost:3412/contatos").then(function (res, status) {
+        $scope.contatos = res.data;
+      });
+    };
+
+    var carregarOperadoras = function () {
+      $http
+        .get("http://localhost:3412/operadoras")
+        .then(function (res, status) {
+          $scope.operadoras = res.data;
+        });
+    };
+
+    $scope.adicionarContato = function (contato) {
+      contato.data = new Date();
+      $http
+        .post("http://localhost:3412/contatos", contato)
+        .then(function (res) {
+          delete $scope.contato;
+          $scope.contatoForm.$setPristine();
+          carregarContatos();
+        });
+    };
+
+    $scope.deletarContatos = function (contatos) {
+      $scope.contatos = contatos.filter(function (contato) {
+        if (!contato.selecionado) return contato;
+      });
+    };
+
+    $scope.isContatoSelecionado = function (contatos) {
+      return contatos.some(function (contato) {
+        return contato.selecionado;
+      });
+    };
+
+    $scope.aplicarFiltroPor = function (campo) {
+      $scope.filtrarPor = campo;
+      $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+    };
+
+    $scope.classe1 = "selecionado";
+    $scope.classe2 = "negrito";
+
+    carregarContatos();
+    carregarOperadoras();
+  });
