8834e812e5835aa35313be6430bd49c67a08bcb9
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2023-06-23 17:59:29 -0300

    Using scope e transclude to make directive re usable

--- js/directives/uiAlertDirective.js
+++ js/directives/uiAlertDirective.js
@@ -1,5 +1,11 @@
 angular.module("listaTelefonica").directive("uiAlert", function () {
   return {
     templateUrl: "view/alert.html",
+    replace: false,
+    restrict: "AE",
+    scope: {
+      title: "@",
+    },
+    transclude: true,
   };
 });
