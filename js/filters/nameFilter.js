3840ec8d575cc30740cdebad71594944aa06c077
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2023-06-23 23:21:44 -0300

    Created filter to mask date

--- js/filters/nameFilter.js
+++ js/filters/nameFilter.js
@@ -2,7 +2,7 @@ angular.module("listaTelefonica").filter("name", function () {
   return function (input) {
     const listaDeNomes = input.split(" ");
     const listaDeNomesFormatada = listaDeNomes.map(function (nome) {
-      if (/^(da|de)$/gi.test(nome)) return nome.toLowerCase();
+      if (/(da|de)/i.test(nome)) return nome.toLowerCase();
       return nome.charAt(0).toUpperCase() + nome.substring(1).toLowerCase();
     });
 
